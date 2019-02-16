f36ee7146093f2f5592215b76f8947e371fdf6f2
Mathew Chan <mail@mathewchan.com>
2019-02-15 20:30:19 -0700

    Setup initial components and scss files

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div className='dashboard'>
+        <h1>Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.scss';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1 className='brand'>CDP Genius</h1>
+      <p>Metamask Status</p>
+    </header>
+  );
+};
+
+export default Header;
