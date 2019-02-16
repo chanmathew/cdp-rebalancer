b8e82001f469f77e04bd98e48083a9882a70a69c
Justin Work <Justin_Work@Justins-MacBook-Pro.local>
2019-02-15 23:14:46 -0700

    added card componet and modified scss for cards and general layout

---
+++ src/components/card/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./styles.scss";
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card block" id="card1">
+        <div className="flex space-between">
+          <h3 className="half">CDP #1</h3>
+          <h5 className="half">wallet address</h5>
+        </div>
+        <div className="progress">progress</div>
+        <div className="flex space-between">
+          <div className="one-fourth">
+            <h6>Collateral</h6>
+            <p>1.3 ETH</p>
+          </div>
+          <div className="one-fourth">
+            <h6>Loan</h6>
+            <p>135.34 DAI</p>
+          </div>
+          <div className="one-fourth">
+            <h6>Liquidation Price</h6>
+            <p>$64.34 / ETH</p>
+          </div>
+          <div className="one-fourth">
+            <h6>Collateral Ratio</h6>
+            <p>254.3%</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import Card from "../card";
+import "./styles.scss";
 
 class Dashboard extends React.Component {
   render() {
     return (
-      <div className='dashboard'>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <h3>Your CDP's</h3>
+        <div className="cardContainer">
+          <Card />
+        </div>
       </div>
     );
   }
